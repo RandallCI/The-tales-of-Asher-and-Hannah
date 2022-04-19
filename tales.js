38ce2ece127dc77aad19447ecc112fdb85d0c196
randallisaacs <sorandall777@gmail.com>
2022-04-19 17:46:59 +0200

    Thank You so much LORD JESUS my Saviour and GOD. I have placed all the story one pages into one folder. I have had to update the way I call referencec but all is well. I am now running through all the pages in the order using a next page button. I have added a bakground image which looks nice but needs to be properly fit. I have removed all the moving items on th pages but they will need to be replaced. Thank You so very much LORD JESUS my GOD.

--- tales.js
+++ tales.js
@@ -1,2 +1,38 @@
 // Page navigation javascript
 
+
+
+
+
+
+//Check if the DOM has been loaded before doing these actions.
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.querySelectorAll('button').forEach(function(button) {
+        button.onclick = function() {
+           if (document.querySelector('#to_page_two')) {
+             window.location.href = 'story_one_page_two.html';
+           } else if (document.querySelector('#to_page_three')) {
+            window.location.href = 'story_one_page_three.html';
+           }  else if (document.querySelector('#to_page_four')) {
+            window.location.href = 'story_one_page_four.html';
+           }  else if (document.querySelector('#to_page_four')) {
+            window.location.href = 'story_one_page_four.html';
+           }  else if (document.querySelector('#to_page_five')) {
+            window.location.href = 'story_one_page_five.html';
+           }  else if (document.querySelector('#to_page_six')) {
+            window.location.href = 'story_one_page_six.html';
+           }  else if (document.querySelector('#to_page_seven')) {
+            window.location.href = 'story_one_page_seven.html';
+           } else {
+            window.location.href = 'story_one_page_one.html';
+           }
+          
+       
+     }
+    
+
+   
+     
+    })
+})
